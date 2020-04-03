d60a5f27e9a3808fa0bd2fee6923b057d2516b6c
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-04-04 02:53:27 +0400

    added Google Sign In functionality

--- app/src/androidTest/java/pl/edu/mimuw/exshare/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/pl/edu/mimuw/exshare/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("pl.edu.mimuw.exshare.mymenu", appContext.getPackageName());
+        assertEquals("pl.edu.mimuw.exshare", appContext.getPackageName());
     }
 }
