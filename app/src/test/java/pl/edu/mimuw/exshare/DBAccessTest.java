daaa68a5183ee26248267efe9cb43636e4d00ab7
kacperkuta <kacperkuta@gmail.com>
2020-04-23 18:08:37 +0200

    Added user registration in database

---
+++ app/src/test/java/pl/edu/mimuw/exshare/DBAccessTest.java
@@ -0,0 +1,61 @@
+package pl.edu.mimuw.exshare;
+
+import org.json.JSONArray;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DBAccessTest {
+    @Test
+    public void addUserTest1() {
+        try {
+            //no result == true assertion here - possibly such user exists.
+            DBAccess.addUser("dummy1");
+            try {
+                assertEquals(1, DBAccess.userExists("dummy1"));
+            } catch (Exception e) {
+                fail("Exception should not be thrown");
+            }
+        } catch (Exception e) {
+            fail("Exception should not be thrown");
+        }
+
+    }
+
+    @Test
+    public void addUserTest2() {
+        try {
+            //no result == true assertion here - possibly such user exists.
+            DBAccess.addUser("dummy2");
+            try {
+                assertEquals(1, DBAccess.userExists("dummy2"));
+            } catch (Exception e) {
+                fail("Exception should not be thrown");
+            }
+        } catch (Exception e) {
+            fail("Exception should not be thrown");
+        }
+    }
+
+    @Test
+    public void userCoursesTest() {
+        try {
+            DBAccess.assignUserToCourse("dummy1", 123);
+        } catch (Exception e) {
+            fail("Exception should not be thrown");
+        }
+    }
+
+    @Test
+    public void userCoursesTest2() {
+        try {
+            JSONArray array = DBAccess.getUserCourses("dummy1");
+        } catch (Exception e) {
+            fail("Exception should not be thrown");
+        }
+    }
+}
