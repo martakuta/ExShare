d68e83a1699cd298fe52ffdb9d2d9fb43452db83
kacperkuta <kacperkuta@gmail.com>
2020-04-23 18:24:04 +0200

    Added user registration in database

--- app/src/test/java/pl/edu/mimuw/exshare/DBAccessTest.java
+++ app/src/test/java/pl/edu/mimuw/exshare/DBAccessTest.java
@@ -4,7 +4,6 @@ import org.json.JSONArray;
 import org.junit.Test;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
