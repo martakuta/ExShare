525d0f43264027e85c7a88bcaf52e507059f0399
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-05-13 06:29:26 +0400

    Image Upload and Download Examples of Adding Getting Images from Firebase Storage

--- app/src/main/java/pl/edu/mimuw/exshare/ExerciseFragment.java
+++ app/src/main/java/pl/edu/mimuw/exshare/ExerciseFragment.java
@@ -1,6 +1,10 @@
 package pl.edu.mimuw.exshare;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
@@ -8,9 +12,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.view.OneShotPreDrawListener;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,20 +27,35 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.firebase.storage.internal.Sleeper;
+import com.squareup.picasso.Picasso;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 
+import static android.app.Activity.RESULT_OK;
+
 
 public class ExerciseFragment extends Fragment {
+    private static final int SELECT_PICTURE = 1;
     private String userID;
     private String userName;
     private int courseID;
     private String courseName;
     private String testName;
     private int exerciseNumber;
+    private ImageView imageView;
+    private FirebaseCloud firebaseCloud = new FirebaseCloud();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,6 +63,48 @@ public class ExerciseFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_exercise, container, false);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Picasso.get().load(data.getData()).noPlaceholder().centerCrop().fit()
+                        .into(imageView);
+            }
+        }
+    }
+   private void pickImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+    private void handleDownloadedImage(Task<byte[]> downloadTask) {
+        downloadTask.addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                setImageView(imageView, bytes);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("[DOWNLOADER]", "Image Download Failed: " + e.getMessage());
+            }
+        });
+    }
+    private void setImageView(ImageView imageView, byte[] bytes) {
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        imageView.setImageBitmap(bitmap);
+    }
+    private void getExampleImage() {
+        Task<byte[]> downloadTask = firebaseCloud.downloadFile("example_directory", "example_task.jpg");
+        downloadTask.addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                setImageView(imageView, bytes);
+            }
+        });
+    }
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @SuppressLint("SetTextI18n")
     @Override
@@ -59,6 +123,39 @@ public class ExerciseFragment extends Fragment {
         courseName = DBAccess.getCourseName(courseID);
         testName = ((MainActivity) requireActivity()).getPresentTestName();
         exerciseNumber = ((MainActivity) requireActivity()).getPresentExerciseNumber();
+        imageView = view.findViewById(R.id.exercise_content);
+
+        // na razie zakładam że użytkownik dodał tego zdjęcia sam
+        getExampleImage();
+
+        view.findViewById(R.id.add_image_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImage();
+            }
+        });
+
+
+        view.findViewById(R.id.upload_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (imageView.getDrawable() == null)
+                    Toast.makeText(requireContext(), "Zdjęcie jest puste", Toast.LENGTH_SHORT).show();
+                else
+                    firebaseCloud.uploadImage("courses/" + courseID + "/" + courseName + "/" + testName, exerciseNumber + ".png", imageView);
+            }
+        });
+
+
+
+        view.findViewById(R.id.download_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Task<byte[]> downloadTask = firebaseCloud.downloadFile("courses/" + courseID + "/" + courseName + "/" + testName, exerciseNumber + ".png");
+                handleDownloadedImage(downloadTask);
+            }
+        });
+
 
         //JSONArray exercise = DBAccess.getExercise(courseID, testName, exerciseNumber); // TODO:: getExercise które zwraca listę: 1. to treść, kolejne to dodane już rozwiązania
         /* TODO::
@@ -68,5 +165,6 @@ public class ExerciseFragment extends Fragment {
          * (ponieważ nie wiadomo ile ich będzie, może 0, to trzeba je tutaj dodawać dynamicznie (to chyba robi się podobnie jak dynamicznie dodawane buttony w klasach
          * YourCourseFragment, CourseFragment i TestFragment)
          */
+        // https://firebasestorage.googleapis.com/v0/b/exshare-1585853870941.appspot.com/o/example_directory%2Fexample.png?alt=media&token=c23e86ed-12af-4b94-b4df-1245c5b05bc5
     }
 }
--- app/src/main/java/pl/edu/mimuw/exshare/FirebaseCloud.java
+++ app/src/main/java/pl/edu/mimuw/exshare/FirebaseCloud.java
@@ -1,32 +1,98 @@
 package pl.edu.mimuw.exshare;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.widget.ImageView;
+
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-public class FirebaseCloud {
+import java.io.ByteArrayOutputStream;
+
+class FirebaseCloud {
     private FirebaseStorage storage = FirebaseStorage.getInstance();
+
+
+    /**
+     * Convert image from ImageView to byte array.
+     * @param imageView ImageView Object.
+     * @return Converted byte[].
+     */
+    private byte[] getImageFromView(ImageView imageView) {
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        return baos.toByteArray();
+    }
+
+    /**
+     * Get associated StorageReference for a given file.
+     * @param path Path to the file.
+     * @param fileName Name of the file.
+     * @return Associated StorageReference Object.
+     */
+    StorageReference getStorageReference(String path, String fileName) {
+        String fullPath = path + "/" + fileName;
+        return storage.getReference().child(fullPath);
+    }
+
+    /**
+     * uploadFile() wrapper with default path="unordered".
+     */
     UploadTask uploadFile(String fileName, byte[] file) {
         return uploadFile("unordered", fileName, file);
     }
-
+    /**
+     * Upload a file to the Firebase Storage.
+     * @param path path to the file.
+     * @param fileName Name of the file.
+     * @param file The file as a byte array.
+     * @return UploadTask that has upload status listeners.
+     */
     UploadTask uploadFile(String path, String fileName, byte[] file) {
-        String fullPath = path+"/"+ fileName;
-        StorageReference fireRef = storage.getReference().child(fullPath);
+        StorageReference fireRef = getStorageReference(path,fileName);
 
         return fireRef.putBytes(file);
     }
-
+    /**
+     * downloadFile() wrapper with default path="unordered".
+     */
     Task<byte[]> downloadFile(String fileName) {
         return downloadFile("unordered", fileName);
     }
 
+    /**
+     * Download a file from Firebase Storage.
+     * @param path path to the file.
+     * @param fileName Name of the file.
+     * @return Task that will allow access to the file with a listener once
+     * the file is downloaded.
+     */
     Task<byte[]> downloadFile(String path, String fileName) {
-        String fullPath = path+"/"+ fileName;
-        StorageReference fireRef = storage.getReference().child(fullPath);
+        StorageReference fireRef = getStorageReference(path,fileName);
 
         return fireRef.getBytes(Long.MAX_VALUE);
     }
+
+    /**
+     * uploadImage() wrapper with default path="unordered".
+     */
+    UploadTask uploadImage(String fileName, ImageView imageView) {
+        return uploadImage("unordered", fileName, imageView);
+    }
+    /**
+     * Upload an Image to Firebase Storage.
+     * @param path path to the file.
+     * @param fileName Name of the image.
+     * @param imageView ImageView Object.
+     * @return UploadTask that has upload status listeners.
+     */
+    UploadTask uploadImage(String path, String fileName, ImageView imageView) {
+        byte[] file = getImageFromView(imageView);
+        return uploadFile(path,fileName,file);
+    }
+
 }
